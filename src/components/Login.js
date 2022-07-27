cb252cb6d6e55ec815bc49cee75fd85566d99525
SeyeongPark <lynn.yeong.102@gmail.com>
2022-07-26 22:59:22 -0400

    implement register feature

--- src/components/Login.js
+++ src/components/Login.js
@@ -6,11 +6,11 @@ function Login() {
         <h1>Login</h1>
             <form method='post'>
                 <div>
-                    <label for='email'>Email</label><br></br>
+                    <label>Email</label><br></br>
                     <input type='text' id='email' name='email' placeholder='Enter Email' required></input>
                 </div>
                 <div>
-                    <label for='password'>Password</label><br></br>
+                    <label>Password</label><br></br>
                     <input type='password' id='password' name='password' placeholder='Enter Password' required></input>
                 </div>
                 <p>Not registered yet? <a href="/register">Create an Account</a> </p>
--- src/components/Register.js
+++ src/components/Register.js
@@ -1,34 +1,104 @@
 import './Login.css';
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+import { useState } from "react";
 
 function Login() {
-  return (
-    <div id='Login-container'>
-        <h1>Register Here</h1>
-            <form method='post'>
-                <div>
-                    <label for='firName'>First Name</label><br></br>
-                    <input type='text' id='firName' name='firName' placeholder='First Name' required></input>
-                </div>
-                <div>
-                    <label for='lastName'>Last Name</label><br></br>
-                    <input type='text' id='lastName' name='lastName' placeholder='Last Name' required></input>
-                </div>
-                <div>
-                    <label for='email'>Email</label><br></br>
-                    <input type='text' id='email' name='email' placeholder='Enter Email' required></input>
-                </div>
-                <div>
-                    <label for='password'>Password</label><br></br>
-                    <input type='password' id='password' name='password' placeholder='Enter Password' required></input>
-                </div>
+	const [error, setError] = useState("");
 
-                <div>
-                    <button type='submit' id='loginBtn' name='loginBtn'>Submit</button>
-                </div>
-            </form>            
-    </div>
+    const [data, setData] = useState({
+        email: '', 
+        password: '',
+        firstName: '',
+        lastName: ''
+    });
 
+    const navigate = useNavigate();
+
+    const handleChange = ({ target: input }) => {
+		setData({ ...data, [input.name]: input.value });
+	};
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        try {
+            const url = "https://team-ggs-back.herokuapp.com/users/signup";
+			const { data: res } = await axios.post(url, data);
+
+            // const dataPost = await fetch(url, {
+            //     method: "POST",
+            //     headers: {
+            //         "Content-Type": "application/json"
+            //       },
+            //     body: JSON.stringify({data})
+            // }).then(res => res.json())
+
+            // setData([...data, dataPost])
+            navigate("/login");
+            // console.log(res.message);
+			console.log(res.message);
+
+        }
+        catch (error) {
+            if (
+				error.response &&
+				error.response.status >= 400 &&
+				error.response.status <= 500
+			) {
+				setError(error.response.data.message);
+			}
+        }
+
+    }
     
+
+    return (
+        <div id='Login-container'>
+            <h1>Register Here</h1>
+                <form onSubmit={handleSubmit}>
+                    <div>
+                        <label>Email</label><br></br>
+                        <input 
+                        type='text' 
+                        name='email' 
+                        placeholder='Enter Email'
+                        onChange={handleChange}
+                        value={data.email} required/>
+                    </div>
+                    <div>
+                        <label>Password</label><br></br>
+                        <input 
+                        type='text' 
+                        name='password' 
+                        placeholder='Enter Password'
+                        onChange={handleChange}
+                        value={data.password} required/>
+                    </div>
+                    <div>
+                        <label>First Name</label><br></br>
+                        <input 
+                        type='text' 
+                        name='firstName' 
+                        placeholder='Enter First Name'
+                        onChange={handleChange}
+                        value={data.firstName} required/>
+                    </div>
+                    <div>
+                        <label>Last Name</label><br></br>
+                        <input 
+                        type='text' 
+                        name='lastName' 
+                        placeholder='Enter Last Name'
+                        onChange={handleChange}
+                        value={data.lastName} />
+                    </div>
+
+                    <div>
+                        <button type='submit' id='loginBtn' name='loginBtn'>Submit</button>
+                    </div>
+                </form>            
+        </div>      
   );
 }
 
